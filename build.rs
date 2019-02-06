9af3a3916bf5805e3ee930b7e1db57cb158a8d4d
Nattapong Sirilappanich <s.nattapong@gmail.com>
2019-02-06 17:40:10 +0700

    Fix typo of function name in #21

--- build.rs
+++ build.rs
@@ -13,10 +13,10 @@ fn implement_tensor_construction() {
         for tar in supported_types {
             f.write_all(format!("
                 ({ty}, $size: expr, $cb: expr) => (
-                    __generate_sized_{ty}_tensor($size, $cb)
+                    generate_sized_{ty}_tensor($size, $cb)
                 );
                 ({ty}, $generator: expr) => (
-                    __generate_unsized_{ty}_tensor($generator)
+                    generate_unsized_{ty}_tensor($generator)
                 );
             ", 
             ty=tar.1
