6b0269a8bef270a02ee546507b5d4095032c1299
Nattapong Sirilappanich <s.nattapong@gmail.com>
2019-02-07 14:13:56 +0700

    Close #22

--- src/lib.rs
+++ src/lib.rs
@@ -72,10 +72,116 @@ include!(concat!(env!("OUT_DIR"), "/pop_tensor.rs"));
 #[proc_macro_hack]
 pub use shape_derive::shape;
 
+/// Utility macro that permit user to specify multiple index
+/// into data for get value at specific index.
+/// If not all the dimension indices was given,
+/// it'll automatically assume to be 0. So if tensor has 3 dimensions
+/// but user call `get!(1; tensor)` then it will semantic equavalent to
+/// `get!(1, 0, 0; tensor)`. 
+/// 
+/// # Example
+/// ```Rust
+/// let tensor = populate_tensor!(f32, 20, |(i, v)| *v = i as f32).view(shape!([2, 5, 2])).unwrap();
+/// assert_eq!(get![0; tensor], 0);
+/// assert_eq!(get![1; tensor], 10);
+/// assert_eq!(get![0, 1; tensor], 2);
+/// assert_eq!(get![0, 2; tensor], 4);
+/// assert_eq!(get![0, 2, 1; tensor], 5);
+/// // uncomment line below to see index out of bound
+/// // get![0, 0, 0, 0; tensor];
+/// ```
+#[macro_export]
+macro_rules! get {
+    ($($i: expr),+; $tensor: expr) => (
+        $tensor[__idx!(0usize, $($i),+; $tensor.shape().1)]
+    );
+}
+
+/// Utility macro that permit user to specify multiple index
+/// into data for set value at specific index.
+/// If not all the dimension indices was given,
+/// it'll automatically assume to be 0. So if tensor has 3 dimensions
+/// but user call `set!(1; tensor; 0)` then it will semantic equavalent to
+/// `set!(1, 0, 0; tensor; 0)`.
+/// 
+/// # Example
+/// ```Rust
+/// let tensor = populate_tensor!(f32, 20, |(_, v)| *v = 99f32).view(shape!([2, 5, 2])).unwrap();
+/// assert_eq!(set![0; tensor; 0f32], tensor[0]);
+/// assert_eq!(set![1; tensor; 10f32], tensor[10]);
+/// assert_eq!(set![0, 1; tensor; 2f32], tensor[2]);
+/// assert_eq!(set![0, 2; tensor; 4f32], tensor[4]);
+/// assert_eq!(set![0, 2, 1; tensor; 5f32], tensor[5]);
+/// // uncomment line below to see index out of bound
+/// // set![0, 0, 0, 0; tensor; 100f32];
+/// ```
+#[macro_export]
+macro_rules! set {
+    ($($i: expr),+; $tensor: expr; $val: expr) => (
+        {
+            let idx = __idx!(0usize, $($i),+; $tensor.shape().1);
+            $tensor.data_mut()[idx] = $val;
+        }
+    );
+}
+
+#[allow(unused)]
+macro_rules! __idx {
+    ($x: expr, $j: expr; $stride: expr) => (
+        $stride[$x] * $j
+    );
+    ($x: expr, $j: expr, $($i: expr),+; $stride: expr) => (
+        ($stride[$x] * $j) + __idx!($x + 1usize, $($i),+; $stride)
+    );
+}
+
 #[cfg(test)]
 mod tests {
     use tensor::*;
+    use storage::*;
     use super::*;
+
+    #[test]
+    fn test_get() {
+        let tensor = populate_tensor!(i8, 20, |(i, v)| {*v = i as i8});
+        let tv = tensor.view(shape!([2, 5, 2])).unwrap();
+        assert_eq!(get!(0, 0, 0; tv), 0);
+        assert_eq!(get!(0; tv), 0);
+        assert_eq!(get!(0, 1; tv), 2);
+        assert_eq!(get!(0, 1, 1; tv), 3);
+        assert_eq!(get!(0, 3, 1; tv), 7);
+        assert!(std::panic::catch_unwind(|| get!(0, 0, 0, 0; tv)).is_err());
+    }
+
+    #[test]
+    fn test_exotic_get() {
+        let mut storage = ByteStorage::new_with_size(12);
+        storage.iter_mut().enumerate().for_each(|(i, v)| *v = i as u8);
+        // simulate a kind of sliding windows on tensor
+        let ts = ByteTensor::new_with_storage_nd(storage, 0, &[2, 5, 2], &[5, 1, 1]);
+        assert_eq!(get!(0, 0, 0; ts), 0);
+        assert_eq!(get!(0; ts), 0);
+        assert_eq!(get!(0, 1; ts), 1);
+        assert_eq!(get!(0, 1, 1; ts), 2);
+        assert_eq!(get!(0, 3, 1; ts), 4);
+        assert!(std::panic::catch_unwind(|| get!(0, 0, 0, 0; ts)).is_err());
+    }
+
+    #[test]
+    fn test_set() {
+        let tensor = populate_tensor!(i8, 20, |(_, v)| *v = 99i8);
+        let mut tv = tensor.view(shape!([2, 5, 2])).unwrap();
+        set!(0, 0, 0; tv; 0);
+        assert_eq!(tv[0], 0);
+        set!(0; tv; 0);
+        assert_eq!(tv[0], 0);
+        set!(0, 1; tv; 2);
+        assert_eq!(tv[2], 2);
+        set!(0, 1, 1; tv; 3);
+        assert_eq!(tv[3], 3);
+        set!(0, 3, 1; tv; 7);
+        assert_eq!(tv[7], 7);
+    }
 
     #[test]
     fn test_populate_fixed_char_tensor() {
